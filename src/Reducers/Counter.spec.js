17040e362b0a66432d73ac4ee525c62253d48663
wanderson <wandersonsc@gmail.com>
2019-08-23 16:31:34 -0300

    Updated test suite

--- src/Reducers/Counter.spec.js
+++ src/Reducers/Counter.spec.js
@@ -4,19 +4,19 @@ import { COUNTER } from "../Actions/types";
 //Test Suites:
 
 describe("Counter Reducer", () => {
-  it("should return the default state", () => {
-    const action = { type: "unexpected" };
+  it("should return the default state when an action type is not passed", () => {
+    const action = {};
     const newState = counter(undefined, action);
     expect(newState.value).toEqual(initState.value);
   });
 
-  it("should increments the counter by 1", () => {
+  it("should increment the counter by 1", () => {
     const action = { type: COUNTER.ADD_VALUE };
     const newState = counter(initState, action);
     expect(newState.value).toBe(initState.value + 1);
   });
 
-  it("should decrements the counter by 1", () => {
+  it("should decrement the counter by 1", () => {
     const action = { type: COUNTER.SUBTRACT_VALUE };
     const newState = counter(initState, action);
     expect(newState.value).toBe(initState.value - 1);
