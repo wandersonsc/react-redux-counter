5d4705075d5fe66da433d3f11727c37145b1ff8a
wanderson <wandersonsc@gmail.com>
2019-09-06 17:33:29 -0300

    Updated Index component

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Container from "@material-ui/core/Container";
 import "../App.css";
-import Home from "./Home/Index";
+import Home from "./Home";
 import Footer from "./Footer";
 
 function App() {
